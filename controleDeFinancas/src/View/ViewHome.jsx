855b5aad6b864c6a4d51e3e1e0bccecf3af493b7
TomCrivellari <tomcrivellari@gmail.com>
2025-05-26 13:22:29 -0300

    Feature: Adição das interfaces iniciais do projeto, a home e a tela de login

--- controleDeFinancas/src/View/ViewHome.jsx
+++ controleDeFinancas/src/View/ViewHome.jsx
@@ -1,11 +1,29 @@
-import {Button, Textarea} from "@heroui/react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 const ViewHome = () => {
 
     return (
-        <div className="flex flex-col items-center justify-center h-auto">
-            <h1>Bem vindo ao controle de finanças</h1>
-            <p>Faça seu login!</p>
+        <div>
+            <Navbar className="bg-[#157347]">
+                <Container>
+                    <Navbar.Brand href="#home" className='text-white'>Controle de Finanças</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text className='text-white'>
+                        Ajuste sua vida financeira conosco
+                    </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className="flex flex-col items-center justify-center h-[40rem] bg-gray-200">
+                <div className='flex flex-col items-center justify-center bg-gray-300 p-8 gap-4 rounded-4xl'>
+                    <p className='text-4xl'>Bem vindo ao controle de finanças</p>
+                    <p className='text-2xl'>Faça seu login!</p>
+                    <Link to="/login" className='bg-[#157347] p-2.5 text-white'>Logar</Link>
+                </div>
+            </div>
         </div>
     )
 }
--- controleDeFinancas/src/View/ViewLogin.jsx
+++ controleDeFinancas/src/View/ViewLogin.jsx
@@ -1,11 +1,40 @@
-
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const ViewLogin = () => {
 
 
     return (
-        <>
-        </>
+        <div>
+            <Navbar className="bg-[#157347]">
+                <Container>
+                    <Navbar.Brand href="#home" className='text-white'>Controle de Finanças</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text className='text-white'>
+                        Ajuste sua vida financeira conosco
+                    </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className='flex flex-col items-center justify-center h-[40rem] bg-gray-200'>
+                <Form className='w-96 flex flex-col items-center justify-center bg-gray-300 h-[20rem] rounded-4xl'>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Usuario</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password"/>
+                    </Form.Group>
+                    <Button variant="success" type="submit">
+                        Logar
+                    </Button>
+                </Form>
+            </div>
+        </div>
     )
 }
 
